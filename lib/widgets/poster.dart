fc6fb5e50469d36c350e1352780da4cd731041da
Simon Bernardin <simon.bernardin@moventes.com>
2019-04-02 21:10:05 +0200

    added an image in assets

--- lib/widgets/poster.dart
+++ lib/widgets/poster.dart
@@ -12,16 +12,16 @@ class Poster extends StatelessWidget {
     if (tmdbService.configuration != null && this.image != null) {
       String path;
       if (this.small) {
-        path = '${tmdbService.configuration.imageBaseUrl}${tmdbService.configuration.posterSizes[0]}${this.image}';
+        path =
+            '${tmdbService.configuration.imageBaseUrl}${tmdbService.configuration.posterSizes[0]}${this.image}';
       } else {
-        path = '${tmdbService.configuration.imageBaseUrl}${tmdbService.configuration.posterSizes[1]}${this.image}';
+        path =
+            '${tmdbService.configuration.imageBaseUrl}${tmdbService.configuration.posterSizes[1]}${this.image}';
       }
       print(path);
-      return Image.network(
-          path,
-          width: 92.0);
+      return Image.network(path, width: 92.0);
     } else {
-      return Container();
+      return Image.asset('assets/images/poster.png');
     }
   }
 }
